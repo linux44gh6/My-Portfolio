134d668208dec49180e360fb6a6ccb43c2d661ac
linuxj4j <abireshan32@gail.com>
2024-06-30 19:11:39 +0600

    add button desing

--- src/app/components/Banner/page.jsx
+++ src/app/components/Banner/page.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { FaArrowDown } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -9,12 +9,12 @@ import { FaDribbble } from "react-icons/fa";
 const BannerPage = () => {
     return (
         <div className='flex justify-around items-center'>
-            <div>
+            <div className=''>
                 <h1 className=' text-yellow-50 text-5xl font-extrabold font-font1'>I am Milon</h1>
                 <h1 className='text-8xl font-extrabold font-font1 bg-gradient-to-r from-color1 to-yellow-50 inline-block text-transparent bg-clip-text'>Web Developer </h1>
-                <p className='w-[500px] text-yellow-50 text-xl '>I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people</p>
+                <p className='w-[500px] text-yellow-50 text-xl mt-4 mb-10'>I break down complex user experinece problems to create integritiy focussed solutions that connect billions of people</p>
                 <div className='flex gap-5 items-center'>
-                    <button className='btn'>Download Resume <span><FaArrowDown></FaArrowDown></span></button>
+                    <button className='btn rounded-full border border-color1 text-color1 bg-color2 hover:text-yellow-50 hover:bg-color1'>Download Resume <span className='text-xl'><MdOutlineFileDownload></MdOutlineFileDownload></span></button>
                     <div className='flex gap-2'>
                     
                    <Link className=' border border-color1 text-color1 hover:bg-color1 hover:text-yellow-50 transition duration-300 p-3 rounded-full' href={''}>
